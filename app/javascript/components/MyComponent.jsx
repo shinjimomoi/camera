11ffa38659310840adb0e45ccec181e0f8e60bab
Ricardo Momoi <shinjimomoi@gmail.com>
2023-05-31 01:15:52 +0900

    react seems to work

--- app/javascript/components/MyComponent.jsx
+++ app/javascript/components/MyComponent.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
 const MyComponent = () => {
-  return <div className='container'>
-          <h1>Hello, React!</h1>
-          <p>This is a test</p>
-        </div>
+  const container = document.getElementById('react-container');
+  const data = JSON.parse(container.getAttribute('data-data'));
+  const { message, user} = data
+
+  return (
+    <div className='container'>
+      <h2>{message}, {user.name}</h2>
+      <p>This is a test</p>
+    </div>
+  );
 };
 
 export default MyComponent;
